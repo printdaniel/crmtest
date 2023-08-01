ac83677fe542a19d7b8992716aad6bc4892fd469
Daniel <dwsuarez98@gmail.com>
2023-08-01 20:42:30 -0300

    formulario de registro de usuario completo

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .form import SignUpForm
 
 
 def home(request):
@@ -22,10 +23,28 @@ def home(request):
             return redirect('home')
 
     else:
-        return render(request, 'home/home.html', {})
+        return render(request, 'home/home.html')
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out...")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfuly Registered!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'home/register.html', {'form': form})
+
+    return render(request, 'home/register.html', {'form': form})
