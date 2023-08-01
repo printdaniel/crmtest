30771929b705e50ff7159985a0ea0ecb087e5007
Daniel <dwsuarez98@gmail.com>
2023-08-01 18:54:13 -0300

    formateado el códig con yapf y flake8

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name = 'home'),
-    path("logout/", views.logout_user, name = 'logout'),
+    path("", views.home, name='home'),
+    path("logout/", views.logout_user, name='logout'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 def home(request):
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
+
         # Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
@@ -16,7 +17,8 @@ def home(request):
             return redirect('home')
 
         else:
-            messages.success(request, "There was An Error Log In, please try again")
+            messages.success(request,
+                             "There was An Error Log In, please try again")
             return redirect('home')
 
     else:
@@ -27,4 +29,3 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out...")
     return redirect('home')
-
