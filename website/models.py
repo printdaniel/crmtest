4735467acd31b9fe855a668bf29432a0e919fa47
Daniel <dwsuarez98@gmail.com>
2023-08-02 15:05:49 -0300

    modelo listo

--- website/models.py
+++ website/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)  # Use EmailField for email addresses
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Record'
+        verbose_name_plural = 'Records'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    # Add a method to display full address
+    def get_full_address(self):
+        return f"{self.address}, {self.city}, {self.state} {self.zipcode}"
+
