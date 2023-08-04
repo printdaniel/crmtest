03786363995fb1784721547a469a78b50818f3b7
Daniel <dwsuarez98@gmail.com>
2023-08-04 14:13:39 -0300

    allowed host

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--k9#*8f+0(rchy!pr&a*y%fc$fh^bc%h@n4=ul!&$(3))==s2f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
