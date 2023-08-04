03519c9f1c46fe40d935cb3f42cc3868e6cd3c30
Daniel <dwsuarez98@gmail.com>
2023-08-04 19:13:32 -0300

    al final solo debug true

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--k9#*8f+0(rchy!pr&a*y%fc$fh^bc%h@n4=ul!&$(3))==s2f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
